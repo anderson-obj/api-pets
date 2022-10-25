ce1af6ef87ef63691bcb4def830dc36d88ef01fd
Anderson Santana <anderson.santana@objective.com.br>
2022-10-24 23:59:19 -0300

    feat:  add routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import * as PageContoller from '../controllers/pageController'
+import * as SearchContoller from '../controllers/searchController'
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('home')
-});
+router.get('/', PageContoller.home);
+router.get('/dogs', PageContoller.dogs);
+router.get('/cats', PageContoller.cats);
+router.get('/fishes', PageContoller.fishes);
+
+router.get('/search', SearchContoller.search);
+
 
 
 export default router;
